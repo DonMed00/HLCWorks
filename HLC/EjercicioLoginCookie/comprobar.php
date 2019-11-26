9e49fa1a3389b76fd459315902e3da975070fc9a
DonMed <xmena97@gmail.com>
2019-11-26 14:41:02 +0100

    1

---
+++ HLC/EjercicioLoginCookie/comprobar.php
@@ -0,0 +1,35 @@
+<html> <head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+ <?php
+ $user = $_POST['user'];
+ $password = $_POST['password'];
+ if(checkUser() && checkPassword()){
+    header ("Location: entrar.php");
+ }else{
+    header ("Location: error.php");
+
+ }
+ ?>
+ <a href="javascript:history.back()"> Volver Atrás</a>
+</body> 
+</html>
+<?php
+function checkUser() {
+    if($user != 'user'){
+        return false;
+    }else{
+        return true;
+    }
+}
+function checkPassword() {
+    if($password != '1234'){
+        return false;
+    }else{
+        return true;
+    }
+
+}
+?>
